46bd134cd8017102a26bacbfdf2e73942193c810
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2019-09-22 15:15:35 +0300

    Revert "Basic 'Day' model structure"
    This reverts commit a39e3cc415a4136f527d6d7a6f217382de414293.

--- models/dayModel.js
+++ models/dayModel.js
@@ -1,23 +1,3 @@
-const mongoose = require('mongoose');
-const Logger = require('../utils/logger/logger.js');
-const logger = new Logger();
-
-const daySchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    date: {
-        type: Date,
-        required: true,
-    },
-    meals: [{
-        type: mongoose.Schema.Tyoes.ObjectId,
-        ref: 'Meal'
-    }],
-    status: {
-        type: String,
-        enum: ['empty', 'in progress', 'finished'],
-        default: 'empty'
-    }
-});
 
 daySchema.post('update', async function () {
     const update = this.getUpdate();
@@ -54,4 +34,3 @@ daySchema.post('update', async function () {
     }
 });
 
-module.exports = mongoose.model('Day', daySchema);
