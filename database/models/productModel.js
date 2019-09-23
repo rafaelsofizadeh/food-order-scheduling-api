9171ad2c9617464465c312ac807400d94f914ac8
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2019-09-24 01:51:07 +0300

    'Product' model

---
+++ database/models/productModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['breakfast', 'brunch', 'lunch', 'dinner', 'vegan', 'vegetarian', 'drink']
+    },
+    quantity: {
+        type: String,
+        validate: {
+            validator: (value) => {
+                return /^\d+?\s[a-zа-я]+?\.?$/.test(value);
+            },
+            message: 'Enter correct quantity format: {empty string} OR {number}{ }{any word, latin or cyrillic}{optional dot}'
+        }
+    },
+    description: String,
+    nutrition: {
+        fat: Number,
+        carbs: Number,
+        protein: Number
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
