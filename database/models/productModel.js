c50778d6ea35dd02fe13bf22b623abb32f183984
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2019-09-25 20:14:08 +0300

    Add productId to 'Product' model

--- database/models/productModel.js
+++ database/models/productModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    id: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true
